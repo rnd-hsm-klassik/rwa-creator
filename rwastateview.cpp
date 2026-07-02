4f3cd36033c3444751a9ffe5c2e98e5ec78344c1
Cedric Spindler <cedric.spindler@fhnw.ch>
2026-07-02 10:22:19 +0200

    Set fixed widths for attribute views and disable horizontal scroll bars
    (cherry picked from commit 31d1cf8581a5f3f31b0bb0e5b49e4fa0991cf8da)

--- rwastateview.cpp
+++ rwastateview.cpp
@@ -17,7 +17,9 @@ RwaStateView::RwaStateView(QWidget* parent, RwaScene *scene, QString name)
 
     assetList = new RwaAssetList(this, scene);
     assetAttributes = new RwaAssetAttributeView(this, scene);
-    assetAttributes->scrollArea->setMaximumWidth(256);
+    assetAttributes->scrollArea->setFixedWidth(260);
+    assetAttributes->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
     currentPoint = nullptr;
     editStateRadius = false;
     editStateHeight = false;
